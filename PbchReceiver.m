cd5b842dab6bd8c7aa681d9815e5b1b10be8768a
Valentin Dymchenko <dymcos2@gmail.com>
2024-07-04 12:07:38 +0300

    PbchReceiver is updated and decrambling had minor changes in names and comments

--- PbchReceiver.m
+++ PbchReceiver.m
@@ -1,6 +1,36 @@
 classdef PbchReceiver
     methods(Static)
         
+        function [MIB, is_data_valid] = receivePbch(bit_sequence, NcellID, Lmax_)
+        %receivePbch Procedure of PBCH deconstruction and MIB extraction [7.1, TS 38.212]
+            arguments
+                bit_sequence (1,:) % demapped and descrambled PBCH bit sequence
+                NcellID (1,1)
+                Lmax_ (1,1) % maximum number of candidate SS/PBCH blocks in half frame [4.1, TS 38.213]
+            end
+            
+            bit_sequence = reverseRateMatching(bit_sequence);
+            bit_sequence = channelDecoding(bit_sequence);
+            [bit_sequence, is_data_valid] = ExtractDataCheckParity(bit_sequence, "crc24c");
+            bit_sequence = descrambling(bit_sequence,NcellID,Lmax_);
+            MIB = payloadReceiving(bit_sequence, Lmax_);
+
+        end
+
+        function out_seq = reverseRateMatching(in_seq)
+            in_seq = reverseRateMatching_bitSelection(in_seq);
+            out_seq = reverseRateMatching_subBlockDeinterleaving(in_seq);
+        end
+
+        function out_seq = channelDecoding(in_seq)
+            in_seq = channelDecoding_polarDecoding(in_seq);
+            out_seq = channelDecoding_deinterleaving(in_seq);
+        end
+
+        function data = payloadReceiving(in_seq,Lmax_)
+            in_seq = payloadReceiving_deinterleaving(in_seq);
+            data = MibParser.ParsePbchPayload(in_seq,Lmax_);
+        end
     end
 
 end
--- descrambling.m
+++ descrambling.m
@@ -1,16 +1,16 @@
-function out_seq = descrambling(in_seq, N_Cell_ID, L_max)
+function out_seq = descrambling(in_seq, NcellID, Lmax_)
     %descrambling Procedure of revererse scrambling
     % after CRC detachment [7.1.2, TS 38.212] 
     arguments
         in_seq (1,:) % input sequence (boolean matrix)
-        N_Cell_ID (1,1)
-        L_max (1,1) % maximum number of candidate SS/PBCH blocks in half frame [5, TS 38.213]
+        NcellID (1,1) % cell ID
+        Lmax_ (1,1) % maximum number of candidate SS/PBCH blocks in half frame [4.1, TS 38.213]
     end
     
     %init
     A = length(in_seq);
     s = zeros(1,A);
-    M = A-3 - (L_max == 10) - 2*(L_max == 20) - 3*(L_max == 64);
+    M = A-3 - (Lmax_ == 10) - 2*(Lmax_ == 20) - 3*(Lmax_ == 64);
     nu = [in_seq(1+6) in_seq(1+24)]; % 3rd & 2nd LSB of SFN are stored in 
     nu = bin2dec(num2str(nu));       % bits 6 & 24 of interleaved sequence
 
@@ -19,7 +19,7 @@ function out_seq = descrambling(in_seq, N_Cell_ID, L_max)
     x2 = zeros(1,2000);
     x1(1) = 1;
     x1(2:31) = 0;
-    x2(1:31) = fliplr(dec2bin(N_Cell_ID,31)); %c_init = N_Cell_ID
+    x2(1:31) = fliplr(dec2bin(NcellID,31)); %c_init = N_Cell_ID
     for n = 1:2000
         x1(n+31) = mod(x1(n+3)+x1(n),2);
         x2(n+31) = mod(x2(n+3)+x2(n+2)+x2(n+1)+x2(n),2);
@@ -32,9 +32,9 @@ function out_seq = descrambling(in_seq, N_Cell_ID, L_max)
     j = 0;
     while i < A
         s_null_cond = i == 6 || i == 24 || ...
-        i == 0 || (i == 31)&&(L_max == 10) || ...
-        ((i == 31)||(i == 29))&&(L_max == 20) || ...
-        ((i == 31)||(i == 29)||(i == 28))&&(L_max == 64);
+        i == 0 || (i == 31)&&(Lmax_ == 10) || ...
+        ((i == 31)||(i == 29))&&(Lmax_ == 20) || ...
+        ((i == 31)||(i == 29)||(i == 28))&&(Lmax_ == 64);
         if  ~s_null_cond
             s(1+i) = c(1+j+nu*M);
             j = j+1;
